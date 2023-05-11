c19e7153f4a464d1f895418a2aa11e9df1355918
Nikita Aleshkin <iessenti@gmail.com>
2023-05-12 01:31:35 +0300

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 
 import path from 'node:path'
 
-import {mockServerConfig} from './mock-server.config';
-import { startMockServer } from 'mock-config-server';
+// import {mockServerConfig} from './mock-server.config';
+// import { startMockServer } from 'mock-config-server';
 
-startMockServer(mockServerConfig)
+// startMockServer(mockServerConfig)
 
 // https://vitejs.dev/config/
 export default defineConfig({
